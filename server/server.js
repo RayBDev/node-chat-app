540e641740a7e08bf3fff5ea98d4c66ff8934ee4
RayB <raybernard1337@gmail.com>
2019-01-24 11:51:15 -0700

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ io.on("connection", socket => {
     users.addUser(socket.id, params.name, params.room);
 
     io.to(params.room).emit("updateUserList", users.getUserList(params.room));
+
     socket.emit(
       "newMessage",
       generateMessage("Admin", "Welcome to the chat app")
@@ -46,16 +47,27 @@ io.on("connection", socket => {
   });
 
   socket.on("createMessage", (message, callback) => {
-    console.log("Create Message", message);
-    io.emit("newMessage", generateMessage(message.from, message.text));
+    let user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit(
+        "newMessage",
+        generateMessage(user.name, message.text)
+      );
+    }
+
     callback();
   });
 
   socket.on("createLocationMessage", coords => {
-    io.emit(
-      "newLocationMessage",
-      generateLocationMessage("Admin", coords.latitude, coords.longitude)
-    );
+    let user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "newLocationMessage",
+        generateLocationMessage(user.name, coords.latitude, coords.longitude)
+      );
+    }
   });
 
   socket.on("disconnect", () => {
